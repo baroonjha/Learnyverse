aaae9fc234a3490f1431c43e3da549f993670907
Ray-linux <ranirava52@gmail.com>
2024-01-27 08:00:30 +0530

    some changes in navber

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,26 +2,25 @@ import React from "react";
 
 function Navbar() {
   return (
-    <div>
-      {/* logo */}
-      <div>logo</div>
-      {/* navbar links */}
-      <ul>
-        <a href="/">
-          <li>Home</li>
-        </a>
-        <a href="/courses"><li>Courses</li></a>
-        <a href="/tutorials"><li>Totorials</li></a>
-        <a href="/notes"><li>Notes</li></a>
-        <a href="/blogs"><li>Blogs</li></a>
-        <a href="/cheatsheets"><li>Cheatsheets</li></a>
-      </ul>
-      {/* navbar buttons */}
-      <div>
-        <button>Login</button>
-        <button>sign up</button>
-      </div>
-    </div>
+    <header className="bg-white">
+      <nav className="flex justify-between items-center w-[92%] bg-red-400">
+        <div>LOGO</div>
+        <div className="">
+          <ul className="flex items-center gap-[4vw]">
+            <li><a href="#" className="hover:text-gray-500">Home</a></li>
+            <li><a href="#" className="hover:text-gray-500">Courses</a></li>
+            <li><a href="#" className="hover:text-gray-500">Tutorials</a></li>
+            <li><a href="#" className="hover:text-gray-500">Notes</a></li>
+            <li><a href="#" className="hover:text-gray-500">Blogs</a></li>
+            <li><a href="#" className="hover:text-gray-500">Cheatsheets</a></li>
+          </ul>
+        </div>
+        <div>
+          <button className="bg-[#0059f1] text-white px-5 py-2 mx-2 rounded-full hover:bg-[#87acec]" >log in</button>
+          <button className="bg-[#0059f1] text-white px-5 py-2 mx-2 rounded-full hover:bg-[#87acec]">sign up</button>
+        </div>
+      </nav>
+    </header>
   );
 }
 
